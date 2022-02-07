b8d08ae089cda02ff934435c052770ccea7de37d
Ali__Shiri <alishiri067@gmail.com>
2022-02-07 07:34:12 -0800

    add navigationComponent to project

--- app/src/main/java/com/example/pickimagefromgallery/MainActivity.kt
+++ app/src/main/java/com/example/pickimagefromgallery/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.example.pickimagefromgallery
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
