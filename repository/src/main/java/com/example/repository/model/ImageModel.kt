891b664305dabd9ba52e5c61612861a8fa251ff8
Ali__Shiri <alishiri067@gmail.com>
2022-02-09 12:53:14 -0800

    edit navGraph

--- repository/src/main/java/com/example/repository/model/ImageModel.kt
+++ repository/src/main/java/com/example/repository/model/ImageModel.kt
@@ -1,5 +1,8 @@
 package com.example.repository.model
 
 import android.graphics.Bitmap
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
-data class ImageModel(val bitmap: Bitmap)
+@Parcelize
+data class ImageModel(val bitmap: Bitmap): Parcelable
